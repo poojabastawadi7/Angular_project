30c0466bed997b680d0a98128a4e8596bc895aa8
poojabastawadi7 <84792394+poojabastawadi7@users.noreply.github.com>
2022-08-16 17:06:56 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-
-
-
-    headers = ['Name','Salary','Experience_year'];
-    employee = [
-    {
-      Name: 'Ram',
-      Salary:'10000',
-      Experience_year:'1',
-
-    },
-    {
-      Name: 'Ravi',
-      Salary:'20000',
-      Experience_year:'2',
-
-      
-    },
-    {
-      Name: 'Raju',
-      Salary:'15000',
-      Experience_year:'1',
-
-      
-    },
-    {
-      Name: 'sid',
-      Salary:'30000',
-      Experience_year:'3',
-    },
-  ];
-
+  title = 'proj';
 }
-    
-
